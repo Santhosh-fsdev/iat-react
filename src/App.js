425c18640b1546818a5174e022699a20e9978654
Santhosh Kumar <sandymech290023@gmail.com>
2020-10-01 15:16:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import axios from "axios";
+import Question from "./Question";
+import Button from "@material-ui/core/Button";
+import Backdrop from "@material-ui/core/Backdrop";
+import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [question, setQuestion] = useState([]);
+  const [load, setLoad] = useState(false);
+  const [newqs, setNewQs] = useState({});
+  const [question1, setQuestion1] = useState("");
+  const [option, setOption] = useState("a");
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      axios({
+        method: "get",
+        url: "http://localhost:8080/allQuestions",
+      })
+        .then((res) => {
+          if (res.data.statusCode === 200) {
+            setQuestion(res.data.payload.data);
+          }
+        })
+        .catch((err) => {
+          window.alert(err.message);
+        });
+    }, 5000);
+
+    return () => clearInterval(intervalId);
+  }, [time]);
+
+  const addQuestion = () => {
+    let payload = {
+      title: question1,
+      a: 0,
+      b: 0,
+      c: 0,
+      d: 0,
+    };
+    if ((option === "a") | "A") {
+      payload.a = 1;
+    }
+    if ((option === "b") | "B") {
+      payload.b = 1;
+    }
+    if ((option === "c") | "C") {
+      payload.c = 1;
+    }
+    if ((option === "d") | "D") {
+      payload.d = 1;
+    }
+    console.log(payload);
+    axios({
+      method: "post",
+      url: `http://localhost:8080/addQuestion`,
+      data: payload,
+    })
+      .then((res) => {
+        window.alert("answer Submitted");
+        setLoad(false);
+      })
+      .catch((err) => {
+        window.alert(err.message);
+        setLoad(false);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IAT-2 Answer Room</h1>
+      <br />
+      <Button variant="contained" color="primary" onClick={() => setLoad(true)}>
+        {" "}
+        Add Qs{" "}
+      </Button>
+
+      <Grid container>
+        {question.length > 0 &&
+          question.map((value, index) => {
+            return <Question value={value} />;
+          })}
+      </Grid>
+      <Modal open={load} onClose={() => setLoad(false)}>
+        <Card>
+          <div style={{ textAlign: "center" }}>
+            <h1>Add Question</h1>
+            <br />
+            <TextField
+              id="outlined-basic1"
+              label="question"
+              value={question1}
+              onChange={(e) => setQuestion1(e.target.value)}
+              variant="outlined"
+            />
+            <br />
+            <br />
+            <TextField
+              id="outlined-basic1"
+              label="Option"
+              value={option}
+              onChange={(e) => setOption(e.target.value)}
+              variant="outlined"
+            />
+            <br />
+            <br />
+            <Button variant="contained" color="primary" onClick={addQuestion}>
+              {" "}
+              Add Qs{" "}
+            </Button>
+          </div>
+        </Card>
+      </Modal>
     </div>
   );
 }
---
+++ src/Question.js
@@ -0,0 +1,86 @@
+import React,{useState} from "react";
+import TextField from '@material-ui/core/TextField';
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import Button from '@material-ui/core/Button';
+import axios from "axios"
+
+function Question(props) {
+
+    
+  const [option,setOption] = useState("none");
+  const [answer,setAnswer] = useState("");
+    const {value} = props
+
+    const answerQuestion = (value) =>{
+        const selected = "";
+        let payload = {
+            title : value.title,
+            a:0,
+            b:0,
+            c:0,
+            d:0
+
+        };
+        if(option === "a" | "A"){
+            payload.a = 1
+        }
+        if(option === "b" | "B"){
+            payload.b = 1
+        }
+        if(option === "c" | "C"){
+            payload.c = 1
+        }
+        if(option === "d" | "D"){
+            payload.d = 1
+        }
+        console.log(payload)
+        axios({
+            method:"post",
+            url:`http://localhost:8080/editQuestion/${value._id}`,
+            data: payload
+        })
+        .then((res)=>{
+            window.alert("answer Submitted")
+        })
+        .catch((err)=>{
+            window.alert(err.message)
+        })
+    }
+  return (
+    <Grid item xs={12} sm={4} className="question">
+      <Card>
+        <div>
+          <p>
+            Question <b>{value.title}</b>
+          </p>
+
+          <TextField
+            id="outlined-basic3"
+            label="Option"
+            value={option}
+            onChange={(e) => setOption(e.target.value)}
+            variant="outlined"
+          />
+          {/* <TextField
+            id="outlined-basic"
+            label="answer"
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            variant="outlined"
+          /> */}
+          <br />
+          <br />
+          <p>Answer count - A (<b>{value.a}</b>) B (<b>{value.b}</b>) C(<b>{value.c}</b>) D(<b>{value.d}</b>)</p>
+          <br />
+          <br />
+          <Button variant="contained" color="primary" onClick={()=>answerQuestion(value)}> Answer </Button>
+          <br />
+          <br />
+        </div>
+      </Card>
+    </Grid>
+  );
+}
+
+export default Question;
